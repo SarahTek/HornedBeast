34cbe6b9503dd0ece3d3d1c450220ba7cc8c63c1
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-04-27 00:47:04 -0400

    changed the image src

--- src/Main.js
+++ src/Main.js
@@ -6,9 +6,9 @@ class Main extends React.Component {
   render() {
     return (
       <>
-      < Hornebeast  title={'Giant Eland'} src={'img/GiantEland.jpg'} description={'the largest species of antelope in the world.'}  />
-      < Hornebeast title={'Noble moose'} src={'img/nooblemoose.jpg'} description={'the largest of all the deer species'}  />
-      < Hornebeast title={'Jacksons Chameleon'} src={'img/jacksons Chameleon'} description={'a species of chameleon native to East Africa, '}  />
+      < Hornebeast  title={'Giant Eland'} src={'https://cdn.pixabay.com/photo/2020/08/23/03/03/unicorn-5509763_960_720.jpg'} description={'flying Horse'}/>
+      < Hornebeast title={'Noble moose'} src={'https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvZmwyMjQ3ODIxMjI0NC1pbWFnZS1rdTJmcHUxbi5qcGc.jpg?s=eYuW9qkt09NpQR4_67PpLRMApAiHwxwC2MOhlniXYAc'} description={'the largest noble moose'} />
+      < Hornebeast title={'Jacksons Chameleon'} src={'https://images.rawpixel.com/image_1000/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL3B4MTI2NTE0OS1pbWFnZS1rd3Z3NWtmNC5qcGc.jpg?s=Z923nKPfkiuHEO9hYDVXxICdkKXRTydip-ecEHdZ1qI'} description={'Cute Animal'}/>
       </>
     )
   }
