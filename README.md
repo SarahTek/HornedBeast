e27904b1e777b8ae9759338afc75cab16be0edb9
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-04-30 21:24:05 -0400

    finished Lab 02

--- README.md
+++ README.md
@@ -1,9 +1,48 @@
+
+Feature #1: Display images
+
 Number and name of feature: #1
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete:  1 hour
 
-Start time: _____
+Start time: 10:30
 
-Finish time: _____
+Finish time: 12:30
 
-Actual time needed to complete: _____
+Actual time needed to complete: 2 hour
+
+
+
+
+Feature #2: Allow users to vote for their favorite beast
+
+Number and name of feature: #1
+
+Estimate of time needed to complete: 30
+
+Start time: 8:40
+
+Finish time: 9:40
+
+Actual time needed to complete: 1:30
+
+
+
+
+
+
+Feature 3: Bootstrap
+
+Number and name of feature: #1
+
+Estimate of time needed to complete: 2 hr
+
+Start time: 5:00
+
+Finish time: 8:30
+
+Actual time needed to complete: 3 hr
+
+
+
+Collaborators: Martha and Natalija
--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import BeastImage from './BeastImage';
-// import Hornedbeast from './Hornedbeast';
+ import BeastImage from './BeastImage';
+//  import Hornedbeast from './Hornedbeast';
+import { Row, Col } from 'react-bootstrap';
 
 class Main extends React.Component {
   render() {
     return (
-      <>
-      <h2>{this.props.message}</h2>
-      <BeastImage image={this.props.ImageUrls[0].image_url} />
-      <BeastImage image={this.props.ImageUrls[1].image_url} />
-      <BeastImage image={this.props.ImageUrls[2].image_url} />
-      <BeastImage image={this.props.ImageUrls[3].image_url} />
-      <BeastImage image={this.props.ImageUrls[4].image_url} />
-      <BeastImage image={this.props.ImageUrls[5].image_url} />
-      <BeastImage image={this.props.ImageUrls[6].image_url} />
-      <BeastImage image={this.props.ImageUrls[7].image_url} />
-      <BeastImage image={this.props.ImageUrls[8].image_url} />
-      <BeastImage image={this.props.ImageUrls[9].image_url} />
-      <BeastImage image={this.props.ImageUrls[10].image_url} />
-      <BeastImage image={this.props.ImageUrls[11].image_url} />
-      <BeastImage image={this.props.ImageUrls[12].image_url} />
-      <BeastImage image={this.props.ImageUrls[13].image_url} />
-      <BeastImage image={this.props.ImageUrls[14].image_url} />
-      <BeastImage image={this.props.ImageUrls[15].image_url} />
-      <BeastImage image={this.props.ImageUrls[16].image_url} />
-      <BeastImage image={this.props.ImageUrls[17].image_url} />
-      <BeastImage image={this.props.ImageUrls[18].image_url} />
-      <BeastImage image={this.props.ImageUrls[19].image_url} />
-  
+      <> 
+        <Row sm={2} md={3} lg={4} >
+
+          {this.props.ImageUrls.map(image => (
+            <Col>
+              <BeastImage
+                key={image._id}
+                title={image.title}
+                imageUrl={image.image_url}
+                description={image.description}
+              />
+            </Col>
+          ))}
+        </Row>
+
       </>
     )
   }
